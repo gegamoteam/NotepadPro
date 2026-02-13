2d6dfbdab4d8f8e0f8f8d1582a66c1b516983258
gegamoteam <gegamo@proton.me>
2026-02-13 19:47:48 +0300

    improvements

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -269,6 +269,7 @@ pub fn run() {
     tauri::Builder::default()
         .manage(WatcherState(Mutex::new(None)))
         .manage(ShortcutStore(Mutex::new(None)))
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
             let _ = app.get_webview_window("main").expect("no main window").show();
